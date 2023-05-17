697922f9250a093722032f7408139073fa61b966
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2023-05-17 23:59:17 +0200

    chore: add title of the project and add style for mobile breakpoint

---
+++ src/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Title from './Title/Title';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <Title />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Home/Title/Title.tsx
@@ -0,0 +1,11 @@
+import './Title.css';
+
+function Title() {
+  return (
+    <h1 className="home_title">
+      GREG<span className="home_title--purple">.</span>GS
+    </h1>
+  );
+}
+
+export default Title;
