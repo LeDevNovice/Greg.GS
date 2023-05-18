b229c3edc0de9bc95d80909f6198fe6b51ac1c9c
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2023-05-18 13:42:28 +0200

    chore: trigger the fading animation

--- src/Home/Title/Title.tsx
+++ src/Home/Title/Title.tsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from 'react';
+
 import './Title.css';
 
 function Title() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    // Set the visibility to true after a short delay to trigger the animation
+    setTimeout(() => {
+      setVisible(true);
+    }, 1000); // Adjust the delay as needed
+  }, []);
+
   return (
-    <h1 className="home_title">
+    <h1 className={`home_title${visible ? '--visible' : ''}`}>
       GREG<span className="home_title--purple">.</span>GS
     </h1>
   );
