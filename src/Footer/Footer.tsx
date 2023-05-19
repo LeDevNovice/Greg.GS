308e3e4af811d371f1927d74ac30da960b2ff068
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2023-05-19 22:00:39 +0200

    chore: add footer component

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <span className="footer-content">Le Dev Novice © 2023</span>
+    </div>
+  );
+}
+
+export default Footer;
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,13 +1,15 @@
 import Title from './Title/Title';
+import Socials from './Socials/Socials';
+import Footer from '../Footer/Footer';
 
 import './Home.css';
-import Socials from './Socials/Socials';
 
 function Home() {
   return (
     <div className="home">
       <Socials />
       <Title />
+      <Footer />
     </div>
   );
 }
