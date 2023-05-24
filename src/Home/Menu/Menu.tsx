7f14d5aa5f256fc15b947f0bfa969846f232ff80
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2023-05-24 20:21:58 +0200

    chore: create new component to display menu of homepage

---
+++ src/Home/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import './Menu.css';
+
+function Menu() {
+  return (
+    <div className="home__menu">
+      <div className="home__menu-item">A propos</div>
+      <div className="home__menu-item">Projets</div>
+      <div className="home__menu-item">Publications</div>
+    </div>
+  );
+}
+
+export default Menu;
