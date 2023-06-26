aee4d0cf3f32277f30040e905c039ba474fb18e5
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2023-06-26 18:22:11 +0200

    feat: add routes definition in the app component with default value for pages other than home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './Home/Home';
 
 function App(): JSX.Element {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<h1>Hello World About</h1>} />
+        <Route path='/projects' element={<h1>Hello World Projects</h1>} />
+        <Route path='/publications' element={<h1>Hello World Publications</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
