7f39d2ec2e904dc3346c9fa621ec7458eb544269
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2023-06-26 18:08:58 +0200

    chore: implement the menu apparition and link to the specific pages

--- src/Home/Menu/Menu.tsx
+++ src/Home/Menu/Menu.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 import './Menu.css';
 
 function Menu() {
   return (
     <div className="home__menu">
-      <div className="home__menu-item">A propos</div>
-      <div className="home__menu-item">Projets</div>
-      <div className="home__menu-item">Publications</div>
+      <Link to="/about" className="home__menu-item">A propos</Link>
+      <Link to="/projects" className="home__menu-item">Projets</Link>
+      <Link to="/publications" className="home__menu-item">Publications</Link>
     </div>
   );
 }
